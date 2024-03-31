b696186d55f344cfe33ba7391750b3da0e2887a6
WahyuAtomic <mangoleho111@com>
2024-03-31 07:37:04 +0100

    merubah

---
+++ app/Controllers/Coba.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controllers;
+
+use PhpParser\Node\Stmt\Echo_;
+
+class Coba extends BaseController
+{
+    public function index();string
+    {
+        Echo "ini Controler Coba";
+    }
+}
